2c406ceb300ebf3d7550ad05eb7226541fa96a88
Samudra Perera <spperera@uwaterloo.ca>
2024-05-04 04:29:08 -0400

    Feat: Added quote and quote source parsing

--- html_to_markdown.py
+++ html_to_markdown.py
@@ -9,7 +9,7 @@ def parse_into_markdown(directory_path):
 
     # Create the file pattern to iterate over
     file_pattern = os.path.join(directory_path, "part0*.xhtml")
-    file_paths = sorted(glob.glob(file_pattern))[9:12]
+    file_paths = sorted(glob.glob(file_pattern))[9:10]
 
     # Iterate over the file_paths
     for file_path in file_paths:
@@ -18,7 +18,7 @@ def parse_into_markdown(directory_path):
             # Parse contents
             soup = BeautifulSoup(content, "html.parser")
 
-            # Fina all divs with element class 'P0'
+            # Find all divs with element class 'P0'
             entries = soup.find_all("div", class_="PO")
 
             for entry in entries:
@@ -28,6 +28,20 @@ def parse_into_markdown(directory_path):
                     # Extracting from h3 element array
                     date = h3.contents[1].strip()
                     title = h3.contents[3].text.strip()
+
+                # Finding the quotes
+                quote = entry.find("p", class_="x03-Chapter-Epigraph").text.strip()
+                quote_2 = entry.find_all("p", class_="x03-Chapter-Epigraph-Verse")
+
+                # Adding subquotations to the original quote as per the book
+                if len(quote_2) > 0:
+                    for n in range(len(quote_2)):
+                        quote += " " + quote_2[n].text.strip()
+
+                # Quote Source
+                quote_source = entry.find(
+                    "p", class_="x03-Chapter-Epigraph-Source"
+                ).text.strip()
 
 
 directory_path = "extracted_contents/OEBPS/Text"
